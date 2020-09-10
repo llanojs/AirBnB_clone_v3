cde03d95252296d8aa50d2faa476a0828c2caf9a
AndrewB4y <andresbaymon@gmail.com>
2020-09-10 21:12:31 +0000

    Adding places_amenities and places_reviews to the available Restful API actions

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,13 +4,14 @@
 
 from os import getenv
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def tear_down(error):
